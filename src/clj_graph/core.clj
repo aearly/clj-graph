b255326328de8b68742f0d9a1a8f5340cf76bd0d
Alexander Early <aearly@fluid.com>
2014-07-13 21:48:43 -0700

    initial commit

---
+++ src/clj_graph/core.clj
@@ -0,0 +1,49 @@
+(ns clj-graph.core
+  (:require
+    [clojure.core :refer :all]
+    [clojure.string :refer [join]]))
+
+(def -re-colon (re-pattern ":"))
+
+(defn vertex-key
+  "abstraction for creating vertex keys from several types of arguments"
+  ([nom id]
+    (join ":" [nom (str id)]))
+  ([arg]
+    (if (vector? arg)
+      (apply vertex-key arg)
+      (if (re-find -re-colon arg)
+        arg
+        (vertex-key "V" arg)))))
+
+
+(defn create []
+  "initializes an empty graph"
+  {
+    "vertices" {}
+    "edges" {}
+    "indexes" {}
+  })
+
+(defn addVertex
+  "add a vertex to the graph"
+  ([graph nom data]
+    (let [verts (get graph "vertices")
+      id (or (get data "id") (str (count verts))) ;if no id, use the number of verts
+      data  (assoc data "id" id)] ; make sure id is included on vertex
+      (assoc graph
+        "vertices" (assoc verts
+          (vertex-key nom id) data)))
+    )
+  ([graph data]
+    "default value for the name is 'V'"
+    (addVertex graph "V" data)))
+
+(defn lastVertex [graph]
+  (get (last (get graph "vertices")) 1))
+
+(defn getVertex
+  ([graph key]
+    (get-in graph ["vertices" key]))
+  ([graph nom id]
+    (getVertex (vertex-key))))
---
+++ test/clj_graph/core_test.clj
@@ -0,0 +1,49 @@
+(ns clj-graph.core-test
+  (:require [clojure.test :refer :all]
+            [clj-graph.core :refer :all]))
+
+(deftest graph-tests
+  (testing "vertices"
+    (testing "should create an empty graph"
+      (let [g (create)]
+        (is (map? g))
+        (is (map? (get g "vertices")))
+        (is (map? (get g "edges")))))
+
+    (testing "should add a named vertex"
+      (let [g (-> (create)
+          (addVertex "foo" {"id" 1 "data" "foo"})
+        )]
+        (is (= (get g "vertices") {"foo:1" {"id" 1 "data" "foo"}}))))
+
+    (testing "should add an id if not included"
+      (let [g (addVertex (create) "foo" {})]
+        (is (= (lastVertex g) {"id" "0"}))))
+
+    (testing "should get a vertex with metadata"
+      (let [vertex {"id" 1 "data" "foo"}
+            g (addVertex (create) "foo" vertex)
+            v (getVertex g "foo" "1")]
+        (is (= v vertex))))
+
+    (testing "should get all vertices with a name")
+  )
+)
+
+
+(deftest vertex-key-tests
+  (testing "should work with 2 args"
+    (is (= (vertex-key "foo" "123") "foo:123"))
+    (is (= (vertex-key "foo" 123) "foo:123")))
+
+  (testing "should work with a single array arg"
+    (is (= (vertex-key ["foo" "123"]) "foo:123")))
+
+  (testing "should work with a single string arg"
+    (is (= (vertex-key "123") "V:123")))
+
+  (testing "should work with an already valid key"
+    (is (= (vertex-key "asdf:123") "asdf:123")))
+
+  (testing "should work with a valid key in an array"
+    (is (= (vertex-key ["asdf:123"]) "asdf:123"))))
